d56ce9b4ad1c5a9809ee77a0ea28e97cbddbc63d
churmeev <churmeev@yoomoney.ru>
2021-12-30 01:03:11 +0300

    test

---
+++ src/main/kotlin/com/bot/service/TestController.kt
@@ -0,0 +1,19 @@
+package com.bot.service
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * TODO javadoc
+ *
+ * @author Churmeev Dmitriy (churmeev@yoomoney.ru)
+ * @since 30.12.2021
+ */
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "test"
+    }
+}
