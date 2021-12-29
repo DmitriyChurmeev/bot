d259b499f74d4d83552ee3c7af045082d6c02140
churmeev <churmeev@yoomoney.ru>
2021-12-30 01:33:03 +0300

    test

--- src/main/kotlin/com/bot/service/TestController.kt
+++ src/main/kotlin/com/bot/service/TestController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("test")
     fun test(): String {
         return "test"
     }
